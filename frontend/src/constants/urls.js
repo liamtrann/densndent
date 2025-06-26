deb9fb1bd5dbe99f20fc49c20bc29f42b2ff8693
liamtrann <hongsoncht@cht.edu.vn>
2025-06-26 09:01:38 -0400

    logo

--- frontend/src/constants/urls.js
+++ frontend/src/constants/urls.js
@@ -1,7 +1,4 @@
-// src/constants/urls.js
-// Centralized URL constants for the app
 
 export const URLS = {
     LOGO: "https://sandbox.densndente.ca/SSP%20Applications/NetSuite%20Inc.%20-%20SCS/SuiteCommerce%20Standard/home-page/Denslogo-sm-f1.png",
-    // Add more URLs here as needed
 };
