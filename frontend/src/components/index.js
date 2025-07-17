07d6005876d87d0f59d949a57dce1a5506fd6ba2
liamtrann <hongsoncht@cht.edu.vn>
2025-07-17 09:04:20 -0400

    clean

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,30 +1,36 @@
 // Cart components
-export { default as CartSummaryPanel } from './cart/CartSummaryPanel';
+export { default as CartConfirmationModal } from './cart/CartConfirmationModal';
 export { default as CartIndicator } from './cart/CartIndicator';
 export { default as CartOrderSummary } from './cart/CartOrderSummary';
 export { default as CartProductCard } from './cart/CartProductCard';
-export { default as CartConfirmationModal } from './cart/CartConfirmationModal';
+export { default as CartSummaryPanel } from './cart/CartSummaryPanel';
+
+// Checkout components
+export { default as CheckoutSummary } from './checkout/CheckoutSummary';
+
+// FAQ components
+export { default as FAQItem } from './faq/FAQItem';
+export { default as FAQs } from './faq/FAQs';
+export { default as FAQSection } from './faq/FAQSection';
+
+// Filter components
+export { default as FilterOption } from './filters/FilterOption';
+export { default as FilterSidebar } from './filters/FilterSidebar';
+
+// Gift card components
+export { default as DndGiftCardSection } from './gift-cards/DndGiftCardSection';
+export { default as EGiftCardSection } from './gift-cards/EGiftCardSection';
+export { default as GiftCardRules } from './gift-cards/GiftCardRules';
+export { default as GiftCardTable } from './gift-cards/GiftCardTable';
 
 // Layout components
+export { default as Banner } from './layout/Banner';
+export { default as Footer } from './layout/Footer';
+export { default as Header } from './layout/Header';
 export { default as LayoutWithCart } from './layout/LayoutWithCart';
+export { default as MainSection } from './layout/MainSection';
 export { default as Modal } from './layout/Modal';
 export { default as VerifyEmailModal } from './layout/VerifyEmailModal';
-export { default as Header } from './layout/Header';
-export { default as Footer } from './layout/Footer';
-export { default as Banner } from './layout/Banner';
-export { default as MainSection } from './layout/MainSection';
-
-// Product components
-export { default as ListProduct } from './product/ListProduct';
-export { default as ListProductComponent } from './product/ListProductComponent';
-export { default as ListProductPage } from './product/ListProductPage';
-export { default as ProductGrid } from './product/ProductGrid';
-export { default as ProductListGrid } from './product/ProductListGrid';
-
-// Promotion components
-export { default as HeroPromotion } from './promotions/HeroPromotion';
-export { default as PromotionGrid } from './promotions/PromotionGrid';
-export { default as PromotionsGrid } from './promotions/PromotionsGrid';
 
 // Navigation components
 export { default as DesktopNav } from './navigation/DesktopNav';
@@ -33,16 +39,12 @@ export { default as MobileDrawer } from './navigation/MobileDrawer';
 export { default as ProductsDropdown } from './navigation/ProductsDropdown';
 export { default as SearchBar } from './navigation/SearchBar';
 
-// Section components
-export { default as BestSellersSection } from './sections/BestSellersSection';
-export { default as BlueBanner } from './sections/BlueBanner';
-export { default as CategoriesSection } from './sections/CategoriesSection';
-export { default as CategoryTiles } from './sections/CategoryTiles';
-export { default as GlovesHighlight } from './sections/GlovesHighlight';
-export { default as HeroCarousel } from './sections/HeroCarousel';
-export { default as MailingList } from './sections/MailingList';
-export { default as Sidebar } from './sections/Sidebar';
-export { default as Catalogues } from './sections/Catalogues';
+// Product components
+export { default as ListProduct } from './product/ListProduct';
+export { default as ListProductComponent } from './product/ListProductComponent';
+export { default as ListProductPage } from './product/ListProductPage';
+export { default as ProductGrid } from './product/ProductGrid';
+export { default as ProductListGrid } from './product/ProductListGrid';
 
 // Profile components
 export { default as ListOrdersHistory } from './profile/ListOrdersHistory';
@@ -52,20 +54,18 @@ export { default as RecentPurchases } from './profile/RecentPurchases';
 export { default as SettingsCard } from './profile/SettingsCard';
 export { default as UserInfoCard } from './profile/UserInfoCard';
 
-// FAQ components
-export { default as FAQItem } from './faq/FAQItem';
-export { default as FAQs } from './faq/FAQs';
-export { default as FAQSection } from './faq/FAQSection';
+// Promotion components
+export { default as HeroPromotion } from './promotions/HeroPromotion';
+export { default as PromotionGrid } from './promotions/PromotionGrid';
+export { default as PromotionsGrid } from './promotions/PromotionsGrid';
 
-// Gift card components
-export { default as DndGiftCardSection } from './gift-cards/DndGiftCardSection';
-export { default as EGiftCardSection } from './gift-cards/EGiftCardSection';
-export { default as GiftCardRules } from './gift-cards/GiftCardRules';
-export { default as GiftCardTable } from './gift-cards/GiftCardTable';
-
-// Filter components
-export { default as FilterOption } from './filters/FilterOption';
-export { default as FilterSidebar } from './filters/FilterSidebar';
-
-// Checkout components
-export { default as CheckoutSummary } from './checkout/CheckoutSummary';
+// Section components
+export { default as BestSellersSection } from './sections/BestSellersSection';
+export { default as BlueBanner } from './sections/BlueBanner';
+export { default as CategoriesSection } from './sections/CategoriesSection';
+export { default as CategoryTiles } from './sections/CategoryTiles';
+export { default as Catalogues } from './sections/Catalogues';
+export { default as GlovesHighlight } from './sections/GlovesHighlight';
+export { default as HeroCarousel } from './sections/HeroCarousel';
+export { default as MailingList } from './sections/MailingList';
+export { default as Sidebar } from './sections/Sidebar';
