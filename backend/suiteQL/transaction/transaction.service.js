a529ddce04f133a9df55c95ce0a6f3b3f1bf106a
liamtrann <hongsoncht@cht.edu.vn>
2025-09-19 11:58:31 -0400

    fix histopry

--- backend/suiteQL/transaction/transaction.service.js
+++ backend/suiteQL/transaction/transaction.service.js
@@ -2,13 +2,13 @@ const { runQueryWithPagination } = require("../util");
 
 class TransactionService {
   async findByEntityId(userId, limit, offset) {
-    let sql = `SELECT t.id, t.entity, ts.name AS status, t.shipcarrier, t.shippingaddress, t.trandate, t.trandisplayname, t.transactionnumber, t.externalid, t.custbody_stc_amount_after_discount AS amountAfterDiscount, t.custbody_stc_tax_after_discount AS taxAfterDiscount, t.custbody_stc_total_after_discount AS totalAfterDiscount, t.foreigntotal, t.totalcostestimate, ea.addrtext AS shipping_addrtext FROM transaction t LEFT JOIN transactionstatus ts ON t.status = ts.id AND t.type = ts.trantype LEFT JOIN entityaddress ea ON ea.nkey = t.shippingaddress WHERE t.entity='${userId}' ORDER BY t.trandate DESC`;
+    let sql = `SELECT t.id, t.entity, ts.name AS status, t.shipcarrier, t.shippingaddress, t.trandate, t.trandisplayname, t.transactionnumber, t.externalid, t.memo, t.custbody_stc_amount_after_discount AS amountAfterDiscount, t.custbody_stc_tax_after_discount AS taxAfterDiscount, t.custbody_stc_total_after_discount AS totalAfterDiscount, t.foreigntotal, t.totalcostestimate, ea.addrtext AS shipping_addrtext FROM transaction t LEFT JOIN transactionstatus ts ON t.status = ts.id AND t.type = ts.trantype LEFT JOIN entityaddress ea ON ea.nkey = t.shippingaddress WHERE t.entity='${userId}' ORDER BY t.trandate DESC`;
     const results = await runQueryWithPagination(sql, limit, offset);
     return results.items || [];
   }
 
   async findByEmail(email, limit, offset) {
-    let sql = `SELECT t.email, t.entity, ts.name AS status, t.shipcarrier, t.shippingaddress, t.trandate, t.trandisplayname, t.transactionnumber, t.externalid, t.custbody_stc_amount_after_discount AS amountAfterDiscount, t.custbody_stc_tax_after_discount AS taxAfterDiscount, t.custbody_stc_total_after_discount AS totalAfterDiscount, t.foreigntotal, t.totalcostestimate, ea.addrtext AS shipping_addrtext FROM transaction t LEFT JOIN transactionstatus ts ON t.status = ts.id AND t.type = ts.trantype LEFT JOIN entityaddress ea ON ea.nkey = t.shippingaddress WHERE t.email='${email}' ORDER BY t.trandate DESC`;
+    let sql = `SELECT t.email, t.entity, ts.name AS status, t.shipcarrier, t.shippingaddress, t.trandate, t.trandisplayname, t.transactionnumber, t.externalid, t.memo, t.custbody_stc_amount_after_discount AS amountAfterDiscount, t.custbody_stc_tax_after_discount AS taxAfterDiscount, t.custbody_stc_total_after_discount AS totalAfterDiscount, t.foreigntotal, t.totalcostestimate, ea.addrtext AS shipping_addrtext FROM transaction t LEFT JOIN transactionstatus ts ON t.status = ts.id AND t.type = ts.trantype LEFT JOIN entityaddress ea ON ea.nkey = t.shippingaddress WHERE t.email='${email}' ORDER BY t.trandate DESC`;
     const results = await runQueryWithPagination(sql, limit, offset);
     return results.items || [];
   }
